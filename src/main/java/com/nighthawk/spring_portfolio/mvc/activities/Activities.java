e280cac88353afe44759532da7722528c4d10131
RohanG326 <gaikwadrohan326@gmail.com>
2023-02-20 14:48:27 -0800

    adding time param to activities

--- src/main/java/com/nighthawk/spring_portfolio/mvc/activities/Activities.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/activities/Activities.java
@@ -1,8 +1,6 @@
 package com.nighthawk.spring_portfolio.mvc.activities;
-
 import java.util.HashMap;
 import java.util.Map;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,46 +8,39 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
-
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
 import com.vladmihalcea.hibernate.type.json.JsonType;
-
 import lombok.NonNull;
-
-
-
 @Entity
 @TypeDef(name = "json", typeClass = JsonType.class)
 public class Activities {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     // email, password, roles are key attributes to login and authentication
     @NotEmpty
-    @Size(min = 5, max = 30)
+    @Size(min = 2, max = 30)
     public String event;
-
     @NonNull
-    @Size(min = 5, max = 30)
+    @Size(min = 2, max = 30)
     public String date;
-
     @NonNull
-    @Size(min = 5, max = 30)
+    @Size(min = 2, max = 30)
+    public String time;
+    @NonNull
+    @Size(min = 2, max = 50)
     public String contact;
-
     @NonNull
-    @Size(min = 5, max = 30)
+    @Size(min = 2, max = 100)
     public String description;
-
     @NonNull
-    @Size(min = 5, max = 30)
+    @Size(min = 5, max = 50)
     public String location;
-
-    public Activities(String event, String date, String contact, String description, String location) {
+    public Activities(String event, String date, String time, String contact, String description, String location) {
         this.event = event;
         this.date = date;
+        this.time = time;
         this.contact = contact;
         this.description = description;
         this.location = location;
@@ -65,6 +56,12 @@ public class Activities {
     }
     public void setDate(String newDate) {
         this.date = newDate;
+    }
+    public String getTime() {
+        return time;
+    }
+    public void setTime(String newTime) {
+        this.time = newTime;
     }
     public String getContact() {
         return contact;
@@ -85,50 +82,42 @@ public class Activities {
         this.location = newLocation;
     }
     public String toString() {
-        return "Activities [id=" + id + ", event=" + event + ", date=" + date + ", contact=" + contact + ", description=" + description
+        return "Activities [id=" + id + ", event=" + event + ", date=" + date + ", time=" + time + ", contact=" + contact + ", description=" + description
                 + ", location=" + location + "]";
     }
     // public static void main(String[] args) {
     //     Activities test = new Activities("golfing", "Feb 7th", "dave", "fun golf day", "thousand oaks golf club");
     //     System.out.println(test.toString());
     // }
-
     public Activities() {
     }
-
-    
     public static Activities[] init() {
-
         // basics of class construction
         Activities p1 = new Activities();
         p1.setEvent("Golfing");
         p1.setDate("Feb 7th");
-        p1.setContact("Davesss");
+        p1.setTime("2pm");
+        p1.setContact("Dave: 858-858-8580");
         p1.setDescription("fun golf day");
         p1.setLocation("thousand oaks golf club");
-
         Activities p2 = new Activities();
         p2.setEvent("Golfing2");
         p2.setDate("Feb 14th");
-        p2.setContact("Dave2ss");
-        p2.setDescription("fun golf day2");
-        p2.setLocation("thousand oaks golf club2");
+        p2.setTime("2pm");
+        p2.setContact("Dave: 858-858-8580");
+        p2.setDescription("fun golf day 2");
+        p2.setLocation("thousand oaks golf club 2");
         // adding Note to notes collection
         // Array definition and data initialization
         Activities activities[] = {p1, p2};
         return(activities);
     }
-
     public static void main(String[] args) {
         // obtain Activities from initializer
         Activities activities[] = init();
-
         // iterate using "enhanced for loop"
         for( Activities test : activities) {
             System.out.println(test);  // print object
         }
     }
 }
-
-
-
--- src/main/java/com/nighthawk/spring_portfolio/mvc/activities/ActivitiesApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/activities/ActivitiesApiController.java
@@ -1,13 +1,10 @@
 package com.nighthawk.spring_portfolio.mvc.activities;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.*;
-
 @RestController
 @RequestMapping("/api/activities/")
 public class ActivitiesApiController {
@@ -15,11 +12,9 @@ public class ActivitiesApiController {
     #### RESTful API ####
     Resource: https://spring.io/guides/gs/rest-service/
     */
-
     // Autowired enables Control to connect POJO Object through JPA
     @Autowired
     private ActivitiesJpaRepository repository;
-
     /*
     GET List of People
      */
@@ -27,7 +22,6 @@ public class ActivitiesApiController {
     public ResponseEntity<List<Activities>> getActivities() {
         return new ResponseEntity<>( repository.findAllByOrderByEventAsc(), HttpStatus.OK);
     }
-
     /*
     GET individual Activities using ID
      */
@@ -39,9 +33,8 @@ public class ActivitiesApiController {
             return new ResponseEntity<>(activity, HttpStatus.OK);  // OK HTTP response: status code, headers, and body
         }
         // Bad ID
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);       
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
-
     /*
     DELETE individual Person using ID
      */
@@ -54,23 +47,22 @@ public class ActivitiesApiController {
             return new ResponseEntity<>(activity, HttpStatus.OK);  // OK HTTP response: status code, headers, and body
         }
         // Bad ID
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST); 
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
-
     /*
     POST Aa record by Requesting Parameters from URI
      */
     @PostMapping( "/post/")
     public ResponseEntity<Object> postActivities(@RequestParam("event") String event,
                                              @RequestParam("date") String date,
+                                             @RequestParam("time") String time,
                                              @RequestParam("contact") String contact,
                                              @RequestParam("description") String description,
                                              @RequestParam("location") String location) {
-        Activities activity = new Activities(event, date, contact, description, location);
+        Activities activity = new Activities(event, date, time, contact, description, location);
         repository.save(activity);
         return new ResponseEntity<>(event +" is created successfully", HttpStatus.CREATED);
     }
-
     /*
     The personSearch API looks across database for partial match to term (k,v) passed by RequestEntity body
      */
@@ -78,18 +70,14 @@ public class ActivitiesApiController {
     public ResponseEntity<Object> personSearch(@RequestBody final Map<String,String> map) {
         // extract term from RequestEntity
         String term = (String) map.get("term");
-
         // JPA query to filter on term
         List<Activities> list = repository.findByEventIgnoreCase(term);
-
         // return resulting list and status, error checking should be added
         return new ResponseEntity<>(list, HttpStatus.OK);
     }
-
     /*
-    The personStats API adds stats by Date to Person table 
+    The personStats API adds stats by Date to Person table
     */
-    
     @GetMapping("/toString/{id}")
     public String activityToString(@PathVariable long id) {
         Optional<Activities> optional = repository.findById(id);
